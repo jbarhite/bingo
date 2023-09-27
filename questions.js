206f46c70ad5f0d5c16acf294d244746f89b1a6c
Justin Barhite <justin.barhite@gmail.com>
2023-09-27 09:45:35 -0600

    Adding first 30 questions (for B and I columns)

---
+++ questions.js
@@ -0,0 +1,39 @@
+questions = [
+	// B
+	[
+		"\\frac{d}{dx} \\left( x^4 \\right)",
+		"\\frac{d}{dx} \\left( x^5 \\right)",
+		"\\frac{d}{dx} \\left( x^8 \\right)",
+		"\\frac{d}{dx} \\left( x^{100} \\right)",
+		"\\frac{d}{dx} \\left( 2x \\right)",
+		"\\frac{d}{dx} \\left( -11x \\right)",
+		"\\frac{d}{dx} \\left( 9x^2 \\right)",
+		"\\frac{d}{dx} \\left( 15x^2 \\right)",
+		"\\frac{d}{dx} \\left( 10x^3 \\right)",
+		"\\frac{d}{dx} \\left( -2x^4 \\right)",
+		"\\frac{d}{dx} \\left( \\sin x \\right)",
+		"\\frac{d}{dx} \\left( \\cos x \\right)",
+		"\\frac{d}{dx} \\left( 3e^x \\right)",
+		"\\frac{d}{dx} \\left( -2e^x \\right)",
+		"\\frac{d}{dx} \\left( \\pi^3 \\right)"
+	],
+
+	// I
+	[
+		"\\frac{d}{dx} \\left( x^3 + 2x^2 - 4x + 5 \\right)",
+		"\\frac{d}{dx} \\left( 2x^4 - 3x^2 + 1 \\right)",
+		"\\frac{d}{dx} \\left( \\frac{3}{x} \\right)",
+		"\\frac{d}{dx} \\left( x^{-1/2} \\right)",
+		"\\frac{d}{dx} \\left( 3x^2 + \\frac{2}{x} \\right)",
+		"\\frac{d}{dx} \\left( -8x^3 + 19x \\right)",
+		"\\frac{d}{dx} \\left( 2x^2 + 5e^x \\right)",
+		"\\frac{d}{dx} \\left( 7 \\sin x - \\cos x \\right)",
+		"\\frac{d}{dx} \\left( 5 \\cos x + 8x \\right)",
+		"\\frac{d}{dx} \\left( \\cos x + 2e^x \\right)",
+		"\\frac{d}{dx} \\left( 3x^{5/3} \\right)",
+		"\\frac{d}{dx} \\left( \\frac{4}{x^2} \\right)",
+		"\\frac{d}{dx} \\left( \\frac{1}{x} + \\frac{1}{x^2} \\right)",
+		"\\frac{d}{dx} \\left( \\sqrt{x^3} \\right)",
+		"\\frac{d}{dx} \\left( \\sqrt{x} - x^5 \\right)"
+	]
+]
