7b6585650c94557a59c973de2df2e5b70a2840f1
Justin Barhite <justin.barhite@gmail.com>
2023-09-29 12:01:49 -0600

    Reworking cards to have null entry for center square

--- cards.py
+++ cards.py
@@ -1,12 +1,25 @@
 import random
 
-for card in range(128):
-	numbers = [[] for i in range(5)]
+
+def generateCard():
+	card = [[] for i in range(5)]
+
 	for c in range(5):
-		for r in range(5):
+		for r in range([5, 5, 4, 5, 5][c]):
 			number = random.randint(0, 14)
-			while number in numbers[c]:
+			while number in card[c]:
 				number = random.randint(0, 14)
-			numbers[c].append(number)
-		numbers[c].sort()
-	print(numbers)
+			card[c].append(number)
+		card[c].sort()
+
+	card[2].insert(2, "null")
+	return card
+
+
+cards = [generateCard() for i in range(128)]
+for card in cards:
+	print("[" + ", ".join([
+		"[" + ", ".join([
+			str(number)
+		for number in col]) + "]"
+	for col in card]) + "]")
